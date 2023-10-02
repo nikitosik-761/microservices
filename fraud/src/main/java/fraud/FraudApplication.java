be9cae2feb4a805f23053849d28cfff350aa132e
nikitosik-761 <ndlysenko2004@gmail.com>
2023-10-02 19:14:01 +0300

    replace eureka-server with k8s

--- fraud/src/main/java/fraud/FraudApplication.java
+++ fraud/src/main/java/fraud/FraudApplication.java
@@ -3,9 +3,17 @@ package fraud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @EnableEurekaClient
+//для k8s
+@PropertySources(
+        {
+                @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+        }
+)
 public class FraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);
--- notification/src/main/java/org/notification/NotificationApplication.java
+++ notification/src/main/java/org/notification/NotificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -14,6 +16,12 @@ import org.springframework.context.annotation.Bean;
         }
 )
 @EnableEurekaClient
+//для k8s
+@PropertySources(
+        {
+                @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
